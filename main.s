5f1eb7c59c9faf74fffa443756b469290d6f85af
JEFF1216 <43406666+GOGOGOGOGOGOG@users.noreply.github.com>
2019-03-11 18:35:17 +0800

    Update main.s

--- main.s
+++ main.s
@@ -6,21 +6,53 @@
 .global _start
 .type _start, %function
 _start:
-	nop
-
 	//
-	//branch w/o link
+	//mov # to reg
 	//
-	b	label01
+	movs	r0,	#250
+	movs	r1,	#250
+	movs	r2,	#102
+	movs	r3,	#103
+	movs    r4, #36
+	movs    r5, #36
+	movs    r6, #190
+	movs    r7, #270
+	movs    r8, #300
+	movs    r9, #360
+    
+	// move reg to reg
+    movs    r0, r5  //check difference between mov and movs 
+	mov     r1, r4
 
-label01:
-	nop
+	// sub 
+	sub     r1, r2 //r1 =-66
+	sub     r4, r5 // r4 =0
+    // reset
+	movs	r0,	#250
+	movs	r1,	#250
+	movs	r2,	#102
+	movs	r3,	#103
 
+
+	// push and pop
 	//
-	//branch w/ link
+    push	{r0, r1, r2, r3}
+	 // reset
+	movs	r0,	#250
+	movs	r1,	#250
+	movs	r2,	#102
+	movs	r3,	#103
+	// push and pop
 	//
-	bl	sleep
+    push	{r1, r2, r0, r3}  //still in compiler is {r0,r1,r2,r3}
 
-sleep:
-	nop
-	b	.
+	// push and pop
+    pop     {r6,r7,r8,r9}   //stack pointer 
+    // push and pop
+	pop     {r8,r7,r9,r6}  //stack pointer getting bigger but still in compiler {r6,r7,r8,r9}
+
+
+
+
+
+
